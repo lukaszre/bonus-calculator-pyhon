460ee6683101d37f8f98c15788461ce6240ca944
michal.lipski <kiwaczki@gmail.com>
2020-05-28 18:48:10 +0200

    first commit

---
+++ src/BonusCalculatorTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+# A testcase is created by subclassing unittest.TestCase.
+class BonusCalculatorTest(unittest.TestCase):
+
+    #tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
+    def test_start_here(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
