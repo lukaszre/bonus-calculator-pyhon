c3e4a97b4cb4780d3b93f4adb3cca79ca06a7a8c
lreszk01 <lukasz.reszka@orange.com>
2020-06-17 16:12:11 +0200

    Individual bonus calculator implementation.

--- src/BonusCalculator.py
+++ src/BonusCalculator.py
@@ -1,11 +1,22 @@
 class BonusCalculator(object):
-    def calulate_bonus(self, sales, quota, commision_pct, tax_pct):
+    @staticmethod
+    def calculate_bonus(sales, quota, commission_pct, tax_pct):
         if tax_pct > 100 or tax_pct < 0:
             raise InvalidTaxException()
-        if commision_pct > 100 or commision_pct < 0:
+        if commission_pct > 100 or commission_pct < 0:
             raise InvalidCommissionException()
         if sales > quota:
-            return (sales-quota)*commision_pct/100*(100 - tax_pct)/100
+            return (sales-quota) * commission_pct / 100 * (100 - tax_pct) / 100
+        return 0
+
+    @staticmethod
+    def calculate_team_bonus(sales, quota, commission_pct, team_members):
+        if commission_pct > 100 or commission_pct < 0:
+            raise InvalidCommissionException
+        if team_members <= 0:
+            raise NoTeamMembersException()
+        if sales > quota:
+            return (sales - quota)*commission_pct/100/team_members
         return 0
 
 
@@ -14,4 +25,8 @@ class InvalidTaxException(Exception):
 
 
 class InvalidCommissionException(Exception):
+    pass
+
+
+class NoTeamMembersException(Exception):
     pass
--- src/BonusCalculatorTest.py
+++ src/BonusCalculatorTest.py
@@ -1,38 +1,51 @@
 import unittest
 
 # A testcase is created by subclassing unittest.TestCase.
-from src.BonusCalculator import BonusCalculator, InvalidTaxException, InvalidCommissionException
+from src.BonusCalculator import BonusCalculator, InvalidTaxException, InvalidCommissionException, NoTeamMembersException
 
 
 class BonusCalculatorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.bonus_calculator = BonusCalculator()
-
     #tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
     def test_nobonus_when_sales_less_than_quota(self):
-        result = self.bonus_calculator.calulate_bonus(12000, 15000, 10, 10)
+        result = BonusCalculator.calculate_bonus(12000, 15000, 10, 10)
         self.assertEqual(0, result)
 
     def test_nobonus_when_sales_equals_quota(self):
-        result = self.bonus_calculator.calulate_bonus(12000, 12000, 10, 10)
+        result = BonusCalculator.calculate_bonus(12000, 12000, 10, 10)
         self.assertEqual(0, result)
 
     def test_bonus_when_sales_grater_then_quota(self):
-        result = self.bonus_calculator.calulate_bonus(12000, 11000, 10, 10)
+        result = BonusCalculator.calculate_bonus(12000, 11000, 10, 10)
         self.assertEqual(90, result)
 
     def test_tax_should_be_less_or_equal_than_100(self):
-        self.assertRaises(InvalidTaxException, self.bonus_calculator.calulate_bonus, 12000, 11000, 10, 101)
+        self.assertRaises(InvalidTaxException, BonusCalculator.calculate_bonus, 12000, 11000, 10, 101)
 
     def test_tax_should_be_grater_or_equal_than_(self):
-        self.assertRaises(InvalidTaxException, self.bonus_calculator.calulate_bonus, 12000, 11000, 10, -1)
+        self.assertRaises(InvalidTaxException, BonusCalculator.calculate_bonus, 12000, 11000, 10, -1)
 
     def test_commission_should_be_less_or_equal_100(self):
-        self.assertRaises(InvalidCommissionException, self.bonus_calculator.calulate_bonus, 12000, 11000, 101, 10)
+        self.assertRaises(InvalidCommissionException, BonusCalculator.calculate_bonus, 12000, 11000, 101, 10)
 
     def test_commission_should_be_grater_or_equal_0(self):
-        self.assertRaises(InvalidCommissionException, self.bonus_calculator.calulate_bonus, 12000, 11000, -1, 10)
+        self.assertRaises(InvalidCommissionException, BonusCalculator.calculate_bonus, 12000, 11000, -1, 10)
+
+
+class TeamBonusCalculatorTest(unittest.TestCase):
+    def test_nobonus_when_sales_less_than_quota(self):
+        result = BonusCalculator.calculate_team_bonus(12000, 15000, 10, 4)
+        self.assertEqual(0, result)
+
+    def test_bonus_when_sales_grater_then_quota(self):
+        result = BonusCalculator.calculate_team_bonus(12000, 11000, 10, 4)
+        self.assertEqual(25, result)
+
+    def test_commission_should_be_less_or_equal_100(self):
+        self.assertRaises(InvalidCommissionException, BonusCalculator.calculate_team_bonus, 12000, 11000, 101, 4)
+
+    def test_teas_should_have_at_least_one_member(self):
+        self.assertRaises(NoTeamMembersException, BonusCalculator.calculate_team_bonus, 12000, 11000, 10, 0)
 
 
 if __name__ == '__main__':
